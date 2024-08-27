72820a48e46f3c7e61aa25d32f9bdf2a42909a0a
Kishan Vyas <kishanvyas308@gmail.com>
2024-08-27 23:39:26 +0530

    client side added

--- packages/ui/src/homepage/displayallproducts.tsx
+++ packages/ui/src/homepage/displayallproducts.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Button } from "../button";
+import { useNavigate } from "react-router-dom";
 
 const products = [
   {
@@ -83,33 +85,42 @@ const products = [
 ];
 
 const DisplayAllProducts: React.FC = () => {
+  const navigate = useNavigate()
   return (
     <div className="w-full h-full px-4 py-3">
-      <div className="text-2xl text-center  font-semibold font-mono">Website For You!</div>
-     
+      <div className="text-2xl text-center  font-semibold font-mono">
+        Website For You!
+      </div>
 
-      <div className="grid  lg:grid-cols-3 md:grid-cols-2 grid-cols-1  xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+      <div className="grid mt-4 pb-7 lg:grid-cols-3 md:grid-cols-2 grid-cols-1  xl:grid-cols-4 2xl:grid-cols-5 gap-4">
         {products.map((product, index) => (
-           <div key={index} className="max-w-[300px] h-full mx-auto w-full md:mx-auto">
-           <div>
-             <div className="bg-gray-200 rounded-lg w-full">  
-               <img
-                 src="https://flowbite.com/docs/images/logo.svg"
-                 className="w-full p-3 "
-               />
-             </div>
+          <div
+            key={index}
+            className="max-w-[300px] h-full mx-auto w-full md:mx-auto"
+            onClick={() => navigate(`/product/${product.id}`)}
+          >
+            <div>
+              <div className="bg-gray-200 rounded-lg w-full">
+                <img
+                  src="https://flowbite.com/docs/images/logo.svg"
+                  className="w-full p-3 "
+                />
+              </div>
 
-             <h3 className="text-xl font-semibold px-1">{product.name}</h3>
-            
-             <div className="flex flex-wrap gap-1 mt-2  justify-start items-center ">
-               {product.tags.map((tag, index) => (
-                 <div key={index} className="bg-gray-200 rounded-md px-2  py-1 text-xs ">
-                   <span>{tag}</span>
-                 </div>
-               ))}
-             </div>
-           </div>
-         </div>
+              <h3 className="text-xl font-semibold px-1">{product.name}</h3>
+
+              <div className="flex flex-wrap gap-1 mt-2  justify-start items-center ">
+                {product.tags.map((tag, index) => (
+                  <div
+                    key={index}
+                    className="bg-gray-200 rounded-md px-2  py-1 text-xs "
+                  >
+                    <span>{tag}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     </div>
---
+++ packages/ui/src/productdescriptionpage/productdescriptionpagebody.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface Product {
+    id: number,
+    name: string,
+    description: string,
+    tags: string[],
+}
+
+const productdescriptionpagebody = (product : Product ) => {
+  return (
+    <div>
+        
+      
+    </div>
+  )
+}
+
+export default productdescriptionpagebody
